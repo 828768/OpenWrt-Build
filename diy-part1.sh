b9ca9beac0eab65df1ecceae7fe4f56165fd7a99
凹凸曼 <zgw505@gmail.com>
2023-01-30 20:27:11 +0800

    update config

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,10 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i "/helloworld/d" "feeds.conf.default"
+sed -i "/openwrt-passwall/d" "feeds.conf.default"
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 cat >>feeds.conf.default <<EOF
-src-git helloworld https://github.com/fw876/helloworld.git
+src-git passwall https://github.com/xiaorouji/openwrt-passwall
 EOF
